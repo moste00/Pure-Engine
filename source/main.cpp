6558ad4b95a8535a99294cbc7f867ad1aa04bdff
nour-aldin <nourdx73@gmail.com>
2022-03-25 20:23:07 +0200

    circle added

--- source/main.cpp
+++ source/main.cpp
@@ -14,8 +14,8 @@ int main(int argc, char** argv) {
     flags::args args(argc, argv); // Parse the command line arguments
     // config_path is the path to the json file containing the application configuration
     // Default: "config/app.json"
-    std::string config_path = args.get<std::string>("c", "config/line02.json");
-    normalizePathToWindows8(config_path);
+    std::string config_path = args.get<std::string>("c", "config/circle02.json");
+    //normalizePathToWindows8(config_path);
     // run_for_frames is how many frames to run the application before automatically closing
     // This is useful for testing multiple configurations in a batch
     // Default: 0 where the application runs indefinitely until manually closed
