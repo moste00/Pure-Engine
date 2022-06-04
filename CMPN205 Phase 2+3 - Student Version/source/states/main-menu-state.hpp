a743cc7f88c825c05d2ecf84c9dd63c6c36c292a
nour-aldin <nourdx73@gmail.com>
2022-06-04 22:26:37 +0200

    Main menu finished

--- CMPN205 Phase 2+3 - Student Version/source/states/main-menu-state.hpp
+++ CMPN205 Phase 2+3 - Student Version/source/states/main-menu-state.hpp
@@ -8,6 +8,10 @@ class MainMenuState: public our::State {
         if(ImGui::Button("Play !",ImVec2(400,200))){
             getApp()->changeState("main") ;
         }
+        else if(ImGui::Button("Exit", ImVec2(400, 200))){
+            glfwDestroyWindow(getApp()->getWindow()); 
+            glfwTerminate(); 
+        }
         ImGui::End();
         glClear(GL_COLOR_BUFFER_BIT);
     }  
